7bff36e8d11ee234f0615b4b655a3a1dac4f8529
George Gbenle <george.gbenle@comms365.com>
2023-05-10 16:23:55 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import CreateProfile from './CreateProfile';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <CreateProfile />
-  </React.StrictMode>
-);
+//router
+import {  createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+//store
+
+import { Provider } from 'react-redux';
+//reducer
+import {store} from './store'
+
+import { IndexRouters } from './router';
+import { SimpleRouter } from './router/simple-router';
+import { ChatRouter } from './router/chat-router';
+
+const router = createBrowserRouter([
+
+  ...IndexRouters,
+  ...SimpleRouter,
+  ...ChatRouter
+], { basename: process.env.PUBLIC_URL })
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+    <App>
+      <RouterProvider router={router}>
+      </RouterProvider>
+      </App>
+    </Provider>
+  </StrictMode>
+)
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter basename={process.env.PUBLIC_URL}>
+//     <Provider store={store}>
+//             <App />
+//         </Provider>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
