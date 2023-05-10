06fd3374e77e06fb182940d6035bc05bf7e9fb30
George Gbenle <george.gbenle@comms365.com>
2023-05-10 16:22:16 +0100

    Remove unwanted code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import logo from './logo.svg';
-import CreateProfile from './CreateProfile';
 
-import './App.css';
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header profile">
-        <img src={logo} className="App-logo" alt="logo" />
-    
-          {/* <p className='profile' onClick={create_profile}>create profile</p> */}
-          <CreateProfile/>
-      
-      </header>
+    <div>
+     
     </div>
   );
 }
