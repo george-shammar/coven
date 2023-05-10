57faece29983c6d1f2fb928135f3ad8684013f65
George Gbenle <george.gbenle@comms365.com>
2023-05-10 16:24:32 +0100

    Implement App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+//router
+// import IndexRouters from "./router/index"
+
+//scss
+import "./assets/scss/socialv.scss"
+import "./assets/scss/customizer.scss"
 
 
-function App() {
+// Redux Selector / Action
+import { useDispatch } from 'react-redux';
+
+// import state selectors
+import { setSetting } from './store/setting/actions'
+
+
+function App(props) {
+  const dispatch = useDispatch()
+  dispatch(setSetting())
+
 
   return (
-    <div>
-     
+    <div className="App">
+      {/* <IndexRouters /> */}
+      {props.children}
     </div>
   );
 }
