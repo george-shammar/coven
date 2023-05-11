59d60f571da2a973d80601e31c5e6c0027019658
George Gbenle <george.gbenle@comms365.com>
2023-05-11 13:38:55 +0100

    Implement unpause scrpi

--- tasks/unpause.ts
+++ tasks/unpause.ts
@@ -1,11 +1,12 @@
-import { task } from 'hardhat/config';
-import { LensHub__factory } from '../typechain-types';
-import { ProtocolState, waitForTx, initEnv, getAddrs } from './helpers/utils';
+const hre = require("hardhat");
+import { ethers } from "ethers";
+// import LensHubAddress from "../../../../contracts/contract-address.json";
+import LensHubArtifact from "../../../../contracts/LensHub.json";
+import LensHubAddress from "../../../../contracts/contract-address.json";
 
-// task('unpause', 'unpauses the protocol').setAction(async ({}, hre) => {
-  async function main() {
-  const [governance] = await initEnv(hre);
-  const addrs = getAddrs();
+async function main() {
+  // const [governance] = await initEnv(hre);
+  // const addrs = getAddrs();
   const lensHub = LensHub__factory.connect(addrs['lensHub proxy'], governance);
   console.log(await lensHub.getState());
   await waitForTx(lensHub.setState(ProtocolState.Unpaused));
