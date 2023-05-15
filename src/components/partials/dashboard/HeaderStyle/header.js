0a99ba43f58b42fd415c896acbba6d5d35949420
George Gbenle <george.gbenle@comms365.com>
2023-05-15 22:24:27 +0100

    Implement pinata

--- src/components/partials/dashboard/HeaderStyle/header.js
+++ src/components/partials/dashboard/HeaderStyle/header.js
@@ -53,6 +53,7 @@ const Header = () => {
   const [formInput, updateFormInput] = useState({handle:""});
   const [fileUrl, setFileUrl] = useState(null);
   const [status, setStatus] = useState("");
+
   
   async function onChange(e) {
     // const file = e.target.files[0];
@@ -103,6 +104,38 @@ const Header = () => {
       });
       } 
   }
+
+    const pinFileToIPFS = async () => {
+        const formData = new FormData();
+        const src = "../../../../assets/images/user/1.jpg";
+        
+        const file = fs.createReadStream(src)
+        formData.append('file', file)
+        
+        const metadata = JSON.stringify({
+          name: 'File name',
+        });
+        formData.append('pinataMetadata', metadata);
+        
+        const options = JSON.stringify({
+          cidVersion: 0,
+        })
+        formData.append('pinataOptions', options);
+
+        try{
+          const res = await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
+            maxBodyLength: "Infinity",
+            headers: {
+              'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+              Authorization: JWT
+            }
+          });
+          console.log(res.data);
+        } catch (error) {
+          console.log(error);
+        }
+    }
+
 
   async function createProfile() {
     const {handle} = formInput;
