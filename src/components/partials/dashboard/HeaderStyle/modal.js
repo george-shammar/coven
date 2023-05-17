c6a3b71e8e5c2fe4c9809d8e46ca6a4fc8d7323d
George Gbenle <george.gbenle@comms365.com>
2023-05-17 18:00:56 +0100

    Add file upload

--- src/components/partials/dashboard/HeaderStyle/modal.js
+++ src/components/partials/dashboard/HeaderStyle/modal.js
@@ -5,6 +5,49 @@ export default function Mode() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [fileUrl, setFileUrl] = useState(null);
+
+  function onFileChange(e) {
+    // Update the state
+    const file = e.target.files[0];
+    console.log(file);
+   // const url = `https://ipfs.infura.io/ipfs/${file}`
+    // Create an object of formData
+    // const formData = new FormData();
+   
+    // Update the formData object
+    // formData.append(
+    //   "myFile",
+    //   fileUrl
+    //   // fileUrl.name
+    // );
+   
+    // Details of the uploaded file
+    
+  };
+
+  function onFileUpload() {
+    // Create an object of formData
+    const formData = new FormData();
+   
+    // Update the formData object
+    formData.append(
+      "myFile",
+      fileUrl,
+      fileUrl.name
+    );
+   
+    // Details of the uploaded file
+    console.log(fileUrl);
+   
+    // Request made to the backend api
+    // Send formData object
+    // axios.post("api/uploadfile", formData);
+  };
+   
+
+
+
   return (
     <>
       <Button className="d-flex align-items-center mt-lg-3" variant="primary" onClick={handleShow}>
@@ -13,7 +56,6 @@ export default function Mode() {
 
       <Modal className="mt-5 pt-5" show={show} onHide={handleClose} size="lg">
         <div className="mt-5 pt-5">
-
         </div>
         <Modal.Header closeButton>
           <Modal.Title>Create Profile</Modal.Title>
@@ -23,7 +65,7 @@ export default function Mode() {
             Every profile is an NFT!
           </p>
           <div>
-            <input type="file" />
+            <input type="file" onChange={onFileChange}/>
           </div>
           
         </Modal.Body>
