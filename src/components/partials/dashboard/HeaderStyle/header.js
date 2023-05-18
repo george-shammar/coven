b5c1da9047cb9dc2e0656a0a2f6aee03b38f3d77
George Gbenle <george.gbenle@comms365.com>
2023-05-18 13:05:24 +0100

    Update display

--- src/components/partials/dashboard/HeaderStyle/header.js
+++ src/components/partials/dashboard/HeaderStyle/header.js
@@ -1238,7 +1238,6 @@ const Header = () => {
 
               
               <Dropdown as="li" className="nav-item user-dropdown">
-              {walletAddress.length > 0 ? (
                 <Dropdown.Toggle
                   href="#"
                   as={CustomToggle}
@@ -1254,24 +1253,7 @@ const Header = () => {
                       <h6 className="mb-0 line-height">{profileHandle}</h6>
                     </div>
                     </Dropdown.Toggle>
-                      ) : (
-
-                        <Dropdown.Toggle
-                        href="#"
-                        as={CustomToggle}
-                        variant="d-flex align-items-center"
-                      >
-                            {/* <Image
-                            src=""
-                            className="img-fluid rounded-circle me-3"
-                            alt="user"
-                            loading="lazy"
-                          /> */}
-                          <div className="caption d-none d-lg-block">
-                            <h6 className="mb-0 line-height">Anon</h6>
-                          </div>
-                          </Dropdown.Toggle>
-                      )}
+                     
                 <Dropdown.Menu className="sub-drop caption-menu">
                   <Card className="shadow-none m-0">
                     <Card.Header>
